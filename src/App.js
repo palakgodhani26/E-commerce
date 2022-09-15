05a8e48e2741630387c93568e795ffaf55609f6f
palakgodhani26 <palakgodhani26@gmail.com>
2022-09-15 18:43:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+// import Home from './Container/Home/Home';
+import { Route } from 'react-router-dom';
+import Shop from './Container/Shop/Shop';
+import Blog from './Container/Blog/Blog';
+import Contact from './Container/Contact/Contact';
+import { store } from '../src/Redux/Store';
+import ToggleThemecontext from './Context/ThemeContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ToggleThemecontext>
+      <Header />
+      {/* <Switch> */}
+        {/* <Route path={"/"} exact component={Home} /> */}
+        <Route path={"/shop"} exact component={Shop} />
+        <Route path={"/blog"} exact component={Blog} />
+        <Route path={"/contact"} exact component={Contact} />
+        {/* <Route path={"/page"} exact component={Page} /> */}
+
+
+      {/* </Switch> */}
+    </ToggleThemecontext>
+    </>
+
   );
 }
 
